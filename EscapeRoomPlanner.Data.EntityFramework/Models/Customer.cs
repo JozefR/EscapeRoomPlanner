0d6506a2a96e8fa7266118714b3c1a032370a0ab
Jozef Randjak <randjakj@bluepartners.cz>
2019-03-15 19:29:51 +0100

    add customer

---
+++ EscapeRoomPlanner.Data.EntityFramework/Models/Customer.cs
@@ -0,0 +1,15 @@
+namespace EscapeRoomPlanner.Data.EntityFramework.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
