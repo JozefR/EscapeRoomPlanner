9386a9f924372164ac69a6a38a6a008a29f80d32
Jozef Randjak <randjakj@bluepartners.cz>
2019-03-16 11:38:09 +0100

    resolved migration

---
+++ EscapeRoomPlanner.Data.EntityFramework/Migrations/20190316103726_RoomToReservationRelation.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EscapeRoomPlanner.Data.EntityFramework.Migrations
+{
+    public partial class RoomToReservationRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RoomId",
+                table: "Reservations",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_RoomId",
+                table: "Reservations",
+                column: "RoomId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_Rooms_RoomId",
+                table: "Reservations",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_Rooms_RoomId",
+                table: "Reservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_RoomId",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "RoomId",
+                table: "Reservations");
+        }
+    }
+}
