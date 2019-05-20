27ca77e1f63e76840b86f44e62678ad9d5e75109
Jozef Randjak <randjakjozef@gmail.com>
2019-05-20 18:56:50 +0200

    update test

---
+++ EscapeRoomPlanner.Data.EntityFramework/Migrations/20190401173126_Test.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EscapeRoomPlanner.Data.EntityFramework.Migrations
+{
+    public partial class Test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Room",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Room",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SecondName",
+                table: "Customer",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Customer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Customer",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Room",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Room",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 1000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SecondName",
+                table: "Customer",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Customer",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Customer",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255);
+        }
+    }
+}
